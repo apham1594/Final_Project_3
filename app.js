45f9ce182ef426fe9e4788993baaff1f9f64f6dc
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-11-28 17:49:40 -0700

    K

---
+++ app.js
@@ -0,0 +1,15 @@
+//Variables for the Tug Of War
+
+var canvas= document.querySelector('canvas')
+var ctx= canvas.getContext('2d')
+var middle= 250
+var position= middle
+
+//Components for the game
+
+function draw () {
+    ctx.clearRect (0,0,500,100);
+
+    ctx.beginPath ();
+
+}
